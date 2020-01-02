c7dac9a5e15c076892b9ed723fe2476f689fd549
Roshan Ghale <ghaleroshan@gmail.com>
2020-01-03 08:51:18 +1100

    Remove Progress Bar

--- src/components/elements/ProgressBar.js
+++ src/components/elements/ProgressBar.js
@@ -1,13 +1,20 @@
-import React from "react"
+import React from "react";
+import Resume from "../../resume.json";
 
 function ProgressBar(props) {
-    return(
-        <div>
-            <span className="title is-5 is-spaced">{props.text}</span>
-            <span className="subtitle is-6 skill-percentage">{props.percentage}%</span>
-            <progress className="progress is-success" value={props.percentage} max="100">{props.percentage}%</progress>
-        </div>
-    )
+  return (
+    <div>
+      <span className="title is-5 is-spaced">{props.text}</span>
+      <span className="subtitle is-6 skill-percentage"></span>
+      {Resume.basics.profiles.map((value, index) => {
+        return (
+          <span className="icon is-medium is-">
+            <i className={value.icon}></i>
+          </span>
+        );
+      })}
+    </div>
+  );
 }
 
-export default ProgressBar
+export default ProgressBar;
